558e6e8a293d2624564a14f7dcd190afbb802cb3
LylyG <lgalarza@spoonflower.com>
2016-02-03 16:20:32 -0500

    Add dealer's card

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -5,6 +5,9 @@ puts "Please enter your first card:"
 puts "Please enter your second card:"
   card2 = gets.chomp.to_i
 
+puts "Please enter the dealer's card:"
+  card3 = gets.chomp.to_i
+
 def player
   (card1 + card2)
 end
