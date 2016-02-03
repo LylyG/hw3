88fd1dc3a0f0fdd4c4db9fe87265d24ce3656554
LylyG <lgalarza@spoonflower.com>
2016-02-03 17:29:04 -0500

    Add hard deck

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -6,8 +6,20 @@ puts "Please enter your second card:"
   card2 = gets.chomp.to_i
 
 puts "Please enter the dealer's card:"
-  card3 = gets.chomp.to_i
+  dealer_card = gets.chomp.to_i
 
-def player
-  (card1 + card2)
+def player(card1, card2)
+  sum = (card1 + card2)
 end
+
+sum = player(card1, card2)
+# puts sum
+
+h = "Hit me!"
+
+hard = { 5 => {2 => h, 10=> h},
+        6 => {2 => h, 3=> h},
+        7 => {2 => h, 3=> h},
+        }
+# puts "#{hard[sum]sum[0]}"
+puts hard[sum][dealer_card]
